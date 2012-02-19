97d24d417ddc7a81794de3a51285db3be76a7b81
misteroo <videordure@yahoo.fr>
2012-02-19 02:53:44 +0100

    add gitignore file

--- module1/src/java/com/misteroo/module1/strategy/impl/LowercaseStrategy.java
+++ module1/src/java/com/misteroo/module1/strategy/impl/LowercaseStrategy.java
@@ -2,6 +2,12 @@ package com.misteroo.module1.strategy.impl;
 
 import com.misteroo.module1.strategy.AbstractStrategy;
 
+/**
+ * Stratégie petite case.
+ * 
+ * @author laurent
+ *
+ */
 public class LowercaseStrategy extends AbstractStrategy {
 
 	@Override
