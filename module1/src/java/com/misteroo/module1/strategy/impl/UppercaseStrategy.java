a926559311ca05d791dbb1d108f1220ef5d4a350
misteroo <videordure@yahoo.fr>
2012-02-19 03:49:06 +0100

    add comment

--- module1/src/java/com/misteroo/module1/strategy/impl/UppercaseStrategy.java
+++ module1/src/java/com/misteroo/module1/strategy/impl/UppercaseStrategy.java
@@ -2,6 +2,12 @@ package com.misteroo.module1.strategy.impl;
 
 import com.misteroo.module1.strategy.AbstractStrategy;
 
+/**
+ * Stratégie grande case.
+ * 
+ * @author laurent
+ *
+ */
 public class UppercaseStrategy extends AbstractStrategy {
 
 	@Override
